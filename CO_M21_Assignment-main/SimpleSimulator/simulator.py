1418a4e4025ecf27124ea3afa29d4c9d31a8189b
tanishqa07 <tanishqa20411@iiitd.ac.in>
2021-08-16 22:22:56 +0530

    basic boiler plate simulator

--- CO_M21_Assignment-main/SimpleSimulator/simulator.py
+++ CO_M21_Assignment-main/SimpleSimulator/simulator.py
@@ -1,1 +1,20 @@
-print(hello)
+import sys
+binary = sys.stdin.read()
+
+registers = {'R0': 0, 'R1': 0, 'R2': 0, 'R3': 0, 'R4': 0, 'R5': 0, 'R6': 0, 'FLAGS':0}
+
+def toBin(a):
+    if(a<0)or(a>255):
+        print("Illegal Immediate values")
+        sys.exit()
+    bn = bin(a).replace('0b','')
+    x = bn[::-1]
+    while len(x) < 8:
+        x += '0'
+    bn = x[::-1]
+    return bn
+
+pc = 0
+halt = False
+
+
